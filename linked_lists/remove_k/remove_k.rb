99de156f2af0631d264418df7a96285faafe1510
Andrew Reid <areid.sj@gmail.com>
2020-11-23 12:53:04 -0700

    Add linked lists folder

---
+++ linked_lists/remove_k/remove_k.rb
@@ -0,0 +1,15 @@
+# Singly-linked lists are already defined with this interface:
+# class ListNode
+#   attr_accessor :value, :next
+#   def initialize(val)
+#     @value = val
+#     @next = nil
+# end
+#
+def removeKFromList(l, k)
+  'hello'
+end
+l = [3, 1, 2, 3, 4, 5]
+k = 3
+
+removeKFromList(l, k)
